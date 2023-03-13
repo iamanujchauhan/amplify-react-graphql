aacc4f509ee302911702e56134627e53e8963d25
anuj chauhan <cchaanuj@amazon.com>
2023-03-13 15:32:49 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <h1>THIS APP IS HOSTED ON AWS AMPLIFY</h1>
+        
+
+
+
       </header>
     </div>
   );
 }
 
 export default App;
+
